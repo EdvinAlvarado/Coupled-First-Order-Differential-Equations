abcf7f1a68b0d03672773553d1173e878ed35e3c
EdvinAlvarado <ealvarado301@gmail.com>
2019-04-10 10:21:00 -0400

    PEP8 compliance

--- two diferential equation solver.py
+++ two diferential equation solver.py
@@ -1,25 +1,25 @@
 import numpy as np
 
-M = [[-0.5, 0.5],[0.5,-0.5]]
+M = [[-0.5, 0.5], [0.5, -0.5]]
 
 a = M[0][0]
 b = M[0][1]
 c = M[1][0]
 d = M[1][1]
 
-print('%s \t %s \n %s \t %s \n' %(a,b,c,d))
+print('%s \t %s \n %s \t %s \n' % (a, b, c, d))
 
 
-#test if real or complex
+# test if real or complex
 if (a+d)**2 - 4*(a*d - b*c) >= 0:
 	r1 = ((a+d) + np.sqrt((a+d)**2 - 4*(a*d - b*c)))/2
 	r2 = ((a+d) - np.sqrt((a+d)**2 - 4*(a*d - b*c)))/2
 else:
 	print('complex number')
-print('roots of equation: %s & %s \n' %(r1,r2))
+print('roots of equation: %s & %s \n' % (r1, r2))
 
-M1 = [[0,0],[0,0]]
-M2 = [[0,0],[0,0]]
+M1 = [[0, 0], [0, 0]]
+M2 = [[0, 0], [0, 0]]
 
 M1[0][0] = a - r1
 M1[1][1] = d - r1
@@ -43,5 +43,5 @@ u2 = [1, -ar2/b]
 # print(u1)
 # print(u2)
 
-print('x1 = (%s)s1 e^(%st) + (%s)s2 e^(%st)' %(u1[0],r1,u2[0],r2))
-print('x2 = (%s)s1 e^(%st) + (%s)s2 e^(%st) \n' %(u1[1],r1,u2[1],r2))
+print('x1 = (%s)s1 e^(%st) + (%s)s2 e^(%st)' % (u1[0], r1, u2[0], r2))
+print('x2 = (%s)s1 e^(%st) + (%s)s2 e^(%st) \n' % (u1[1], r1, u2[1], r2))
